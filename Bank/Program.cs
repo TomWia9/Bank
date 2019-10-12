2859ace1fda145d3d028d40f5f00613be1f0be27
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2019-10-12 18:49:46 +0200

    End.

--- Bank/Program.cs
+++ Bank/Program.cs
@@ -7,22 +7,8 @@ namespace Bank
     {
         static void Main(string[] args)
         {
-            AccountsManager manager = new AccountsManager();
-
-            manager.CreateBillingAccount("Tomasz", "Wiatrowski", 1234567890);
-            manager.CreateSavingsAccount("Tomasz", "Wiatrowski", 1234567890);
-            manager.CreateSavingsAccount("Karolina", "Chruścińska", 0987654321);
-
-            IEnumerable<string> users = manager.ListOfCustomers();
-
-            Printer printer = new Printer();
-
-            foreach (var item in users)
-            {
-                Console.WriteLine(item);
-            }
-
-            Console.ReadKey();
+            BankManager bankManager = new BankManager();
+            bankManager.Run();
         }
     }
 }
